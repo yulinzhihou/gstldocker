4cc371fd868a64350009d574d788bd3570b5b684
yulinzhihou <yulinzhihou@163.com>
2025-05-21 16:33:12 +0800

    增加初始化数据库脚本-1

--- services/gsmysql80/include/init_db.sh
+++ services/gsmysql80/include/init_db.sh
@@ -1,3 +1,4 @@
 # 初始化数据库
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -h127.0.0.1 -P3306 tlbbdb </docker-entrypoint-initdb.d/tlbbdb.sql
 mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -h127.0.0.1 -P3306 web </docker-entrypoint-initdb.d/web.sql
+mysql -uroot -p"${MYSQL_ROOT_PASSWORD}" -h127.0.0.1 -P3306 gs_gmtools </docker-entrypoint-initdb.d/gs_gmtools.sql
