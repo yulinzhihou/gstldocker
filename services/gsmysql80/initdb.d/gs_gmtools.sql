68daeb87b39beac63c349118557b5556fc15390b
yulinzhihou <yulinzhihou@163.com>
2025-05-22 10:31:38 +0800

    增加GM工具数据表字段

--- services/gsmysql80/initdb.d/gs_gmtools.sql
+++ services/gsmysql80/initdb.d/gs_gmtools.sql
@@ -1,12 +1,13 @@
 use gs_gmtools;
 
-CREATE TABLE `event_list` (
+CREATE TABLE if not exists `event_list` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `event` varchar(255) NOT NULL COMMENT '事件标识',
   `note` varchar(255) NOT NULL COMMENT '事件说明',
   `create_time` datetime DEFAULT NULL COMMENT '事件创建时间',
   `status` tinyint(2) unsigned NOT NULL DEFAULT '0' COMMENT '执行状态',
   `request_time` datetime DEFAULT NULL COMMENT '事件请求回执时间',
+  `name` varchar(255) DEFAULT NULL COMMENT '名称',
   `param1` varchar(255) DEFAULT NULL COMMENT '参数1',
   `param2` varchar(255) DEFAULT NULL COMMENT '参数2',
   `param3` varchar(255) DEFAULT NULL COMMENT '参数3',
